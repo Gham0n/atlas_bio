a97d46179c7b0099b2dd264ba837b693323466d8
Kantun35 <quentin.bigot35830@gmail.com>
2023-11-26 17:09:33 +0100

    internet permission

--- app/src/main/java/com/example/atlas_bio/MapFicheFragment.java
+++ app/src/main/java/com/example/atlas_bio/MapFicheFragment.java
@@ -1,14 +1,19 @@
 package com.example.atlas_bio;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.NotNull;
 import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -19,6 +24,7 @@ import java.util.regex.Pattern;
 
 public class MapFicheFragment extends Fragment {
 
+    private static final int REQUEST_INTERNET_PERMISSION = 1;
     String coordonneeGPS;
 
     float longitude;
@@ -42,10 +48,22 @@ public class MapFicheFragment extends Fragment {
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        //CreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.map_fiche_fragment, container, false);
         this.coordonneeGPS = getArguments().getString("coordonneeGPS");
         this.map = (MapView) view.findViewById(R.id.map);
+
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.INTERNET}, REQUEST_INTERNET_PERMISSION);
+        } else {
+
+            Configuration.getInstance().setDebugMode(true);
+            initializeMap();
+
+        }
+        return view;
+    }
+
+    public void initializeMap() {
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         map.setBuiltInZoomControls(true);
@@ -56,7 +74,6 @@ public class MapFicheFragment extends Fragment {
         setCoordonnee();
         GeoPoint startPoint = new GeoPoint(latitude, longitude);
         mapController.setCenter(startPoint);
-        return view;
     }
 
     private void setCoordonnee() {
@@ -70,6 +87,17 @@ public class MapFicheFragment extends Fragment {
             longitude = Float.parseFloat(matcher.group(0));
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_INTERNET_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initializeMap();
+            }
+        }
     }
 
 
