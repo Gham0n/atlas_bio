f9f8352d6a86c68b4b2d6415beeb6c9f25c836d7
Kantun35 <quentin.bigot35830@gmail.com>
2023-11-27 11:28:47 +0100

    ne peux pas acceder au fichiers externes

--- app/src/main/java/com/example/atlas_bio/MapFicheFragment.java
+++ app/src/main/java/com/example/atlas_bio/MapFicheFragment.java
@@ -1,7 +1,9 @@
 package com.example.atlas_bio;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,17 +16,19 @@ import androidx.fragment.app.Fragment;
 import org.jetbrains.annotations.NotNull;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.library.BuildConfig;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
+import java.io.File;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MapFicheFragment extends Fragment {
 
-    private static final int REQUEST_INTERNET_PERMISSION = 1;
+    private static final int REQUEST_CODE_PERMISSION = 1;
     String coordonneeGPS;
 
     float longitude;
@@ -52,8 +56,14 @@ public class MapFicheFragment extends Fragment {
         this.coordonneeGPS = getArguments().getString("coordonneeGPS");
         this.map = (MapView) view.findViewById(R.id.map);
 
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.INTERNET}, REQUEST_INTERNET_PERMISSION);
+        String[] permissions = {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET
+        };
+
+        if (!checkMultiplePermissions(requireContext(),permissions)) {
+            ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_CODE_PERMISSION);
         } else {
 
             Configuration.getInstance().setDebugMode(true);
@@ -64,6 +74,8 @@ public class MapFicheFragment extends Fragment {
     }
 
     public void initializeMap() {
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         map.setBuiltInZoomControls(true);
@@ -92,12 +104,38 @@ public class MapFicheFragment extends Fragment {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean isPerm = true;
 
-        if (requestCode == REQUEST_INTERNET_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initializeMap();
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if(grantResults.length > 0) {
+                for (int grantResult : grantResults) {
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        isPerm = false;
+                    }
+                }
+            } else {
+                isPerm = false;
+            }
+        } else {
+            isPerm = false;
+        }
+
+        if(isPerm) {
+            File osmdroidTilesDirectory = new File("/storage/emulated/0/osmdroid/tiles");
+            if (!osmdroidTilesDirectory.exists()) {
+                osmdroidTilesDirectory.mkdirs();
+            }
+            initializeMap();
+        }
+    }
+
+    public static boolean checkMultiplePermissions(Context context, String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
             }
         }
+        return true;
     }
 
 
