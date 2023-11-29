c1fec3bb95729a1c85d2cec2cdfcd47d940965fb
Gui <guillaumehamon.gh@gmail.com>
2023-11-29 17:42:48 +0100

    Modif de l'user page

--- app/src/main/java/com/example/atlas_bio/UserProfile.java
+++ app/src/main/java/com/example/atlas_bio/UserProfile.java
@@ -5,12 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -37,8 +46,31 @@ public class UserProfile extends Fragment {
         TextView name = view.findViewById(R.id.userProfile_name);
         TextView email = view.findViewById(R.id.userProfile_email);
 
-        //name.setText(firebaseUser.getDisplayName());
         email.setText(firebaseUser.getEmail());
+
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("users");
+        Query query = databaseRef.orderByChild("email").equalTo(firebaseUser.getEmail());
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    User user = userSnapshot.getValue(User.class);
+                    if (user != null) {
+                        // L'utilisateur a été trouvé, vous pouvez accéder à ses propriétés ici
+                        String userName = user.getName();
+                        name.setText(userName);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Gestion des erreurs
+            }
+        });
+
+
 
         return view;
     }
